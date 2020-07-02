53fd5c709d655cb31292d0a993fe55dd34a9d044
DYagmur <30656517+DYagmur@users.noreply.github.com>
2020-07-02 16:07:25 +0300

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,16 @@
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount + 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "Spam " * spam_amount
+print(viking_song)
+
+
+#Variable assignment: Here we create a variable called spam_amount and assign it the value of 0 using =, which is called the assignment operator.
+#Function calls:. print is a Python function that displays the value passed to it on the screen. We call functions by putting parentheses after their name, and putting the inputs (or arguments) to the function in those parentheses.
+#The first line above is a comment. In Python, comments begin with the # symbol.
